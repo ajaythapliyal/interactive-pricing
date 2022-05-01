1d4eac9fdec498c7090821e30680454fad683844
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-05-01 18:53:41 +0530

    toggle

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-use-before-define */
-;(() => {
+// eslint-disable-next-line no-unused-vars
+const interactive = (() => {
     const slider = document.querySelector('.progress-bar')
     const progress = document.querySelector('.progress')
+    const toggleCircle = document.querySelector('.toggle-circle')
 
     slider.onmousedown = (event) => {
-        const pct = progressPct(event.pageX)
-        progress.style.width = `${pct}%`
-
+        progress.style.width = `${progressPct(event.pageX)}%`
         document.onmousemove = (moveEvent) => {
             progress.style.width = `${progressPct(moveEvent.pageX)}%`
         }
@@ -20,5 +20,19 @@
     function progressPct(touchLoc) {
         const pct = ((touchLoc - slider.offsetLeft) / slider.offsetWidth) * 100
         return pct > 100 ? 100 : pct < 0 ? 0 : pct
+    }
+
+    function toggle() {
+        if (toggleCircle.classList.contains('toggle-monthly')) {
+            toggleCircle.classList.remove('toggle-monthly')
+            toggleCircle.classList.add('toggle-yearly')
+        } else {
+            toggleCircle.classList.remove('toggle-yearly')
+            toggleCircle.classList.add('toggle-monthly')
+        }
+    }
+
+    return {
+        toggle,
     }
 })()
